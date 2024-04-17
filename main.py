f0756debe028bc6739849df278698c4842f0ae29
HubTakenIs <hubertaslol@gmail.com>
2024-04-17 15:14:13 +0100

    added main function

--- main.py
+++ main.py
@@ -8,40 +8,21 @@ from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
-stop_words = set(stopwords.words('english'))
 
-ps = PorterStemmer()
-fp = open("reddit_jokes.json","r",1)
 
-jayson = json.load(fp)
 
-print(len(jayson)) # 194553 reddit jokes
-#print(jayson[2]) # first not racist joke
-
-mytest = jayson[2]
-
-#print(type(mytest))
-
-# vocabulary
-vocabulary = {}
-
-punctuationList = string.punctuation
-
-def removePunctuation(text):
+def removePunctuation(text,punctuationList):
     for punct in punctuationList:
-        #print(f"trying to replace {punct}")
         text = text.replace(punct,"")
     text = text.replace("\n"," ")
     text = text.replace('“',' ')
     text = text.replace('”',' ')
     text = text.replace('’',' ')
     text = text.replace('‘',' ')
-
-
     return text
 
 
-def textToIndex(text):
+def textToIndex(text,stop_words,stemmer):
     splitText = word_tokenize(text)
     count = 0
     jokeIndex = {}
@@ -51,7 +32,7 @@ def textToIndex(text):
             count +=1
             continue
         else:
-            stemmed = ps.stem(word)
+            stemmed = stemmer.stem(word)
             if stemmed in jokeIndex.keys():
                 jokeIndex[stemmed].append(count)
             else:
@@ -61,72 +42,53 @@ def textToIndex(text):
     return jokeIndex
 
 
-def addIndexToVocabulary(index, jid):
+def addIndexToVocabulary(index, jid,vocabulary):
     for term in index:
         if term in vocabulary.keys():
             vocabulary[term][jid] = index[term]
         else:
             vocabulary[term] = {jid:index[term]}
 
-body = mytest['body']
 
-<<<<<<< HEAD
-for i in range(0,10):
-    # set as key of dict
-    maindict = {}
-    maindict[("term", 1)] = ({"json":"object"},2)
-    maindict[("not", 2)] = ({"json":"object"},2)
-    print(maindict)
-    
-=======
-
-
-
-for i in range(0,11):
->>>>>>> 6c116d73c17c3f4117c0257d450b859d01b32e90
-    #retrieve joke
-    joke = jayson[i]
-    jid = joke['id']
-    # retrieve text from joke
-    title = joke['title']
-    body = joke['body']
-    # remove punctuation
-    title = removePunctuation(title)
-    body = removePunctuation(body)
-<<<<<<< HEAD
-
-    joke_text = title + " " + body
-
-    
-
-    joke_index = textToIndex(joke_text)
-
-    # normalised Term Frequency
-    # tf = term occurance / most occuring term
-    # tf per document?
-    
-    # joke index
-
-    addIndexToVocabulary(joke_index,jid)
-    
+def main():
+    stop_words = set(stopwords.words('english'))
+    punctuationList = string.punctuation
+    ps = PorterStemmer()
+    fp = open("reddit_jokes.json","r",1)
+    jayson = json.load(fp)
+    print(len(jayson)) # 194553 reddit jokes
+    #print(jayson[2]) # first not racist joke
+    mytest = jayson[2]
+    #print(type(mytest))
+    # vocabulary
+    vocabulary = {}
+    body = mytest['body']
+    for i in range(0,11):
+        #retrieve joke
+        joke = jayson[i]
+        jid = joke['id']
+        #retrieve text from joke
+        title = joke['title']
+        body = joke['body']
+        # remove punctuation
+        title = removePunctuation(title,punctuationList)
+        body = removePunctuation(body,punctuationList)
+        joke_text = title + " " + body
+        joke_index = textToIndex(joke_text,stop_words,ps)
+        # normalised Term Frequency
+        # tf = term occurance / most occuring term
+        # tf per document?
+        # joke index
+        addIndexToVocabulary(joke_index,jid,vocabulary)
     #print("JOKE INDEX ")
     #print(joke_index)
-=======
-    joke1 = title + " " + body
-    
-    #create each index
-    joke_index = textToIndex(joke1)
-    #addIndexToVocabulary
-    addIndexToVocabulary(joke_index,jid)
-    
->>>>>>> 6c116d73c17c3f4117c0257d450b859d01b32e90
-    
-
-
     if i % 10000 == 0:
         print("10k processed")
+    f = open("Main-Output.txt","w")
+    f.write(str(vocabulary))
+    f.close()
+    fp.close()
 
-f = open("Main-Output.txt","w")
-f.write(str(vocabulary))
-f.close()
-#print(vocabulary)
+
+if __name__ == "__main__":
+    main()
