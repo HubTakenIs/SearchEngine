620501d448c6e7ee1717aa0b4940cde33b6b5a40
HubTakenIs <hubertaslol@gmail.com>
2024-04-27 16:31:19 +0100

    telling flask app it's behind a proxy

--- api-backend/server.py
+++ api-backend/server.py
@@ -4,8 +4,13 @@ from flask import request
 from flask import render_template
 from main import QueryToDocVector, loadDocuments, executeQuery
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1,x_host=1,x_prefix=1
+)
 documentVectors = loadDocuments("documentVectorSpace.bin")
 documents = loadDocuments("documents.bin")
 
@@ -21,14 +26,12 @@ def joke_search(query=None):
     query = request.args.get('query')
     queryVector = QueryToDocVector(query)
     output = executeQuery(queryVector,documentVectors,documents)
-    #top5 = []
     response = ""
     for i in range(5):
         response += f"<div class='panel'><h2>{output[i][2]['title']}</h2>"
         response += f"<div ><p>{output[i][2]['body']}</p> </div></div>"
 
     return response
-    #return json.dumps(top5)
 
 
  
